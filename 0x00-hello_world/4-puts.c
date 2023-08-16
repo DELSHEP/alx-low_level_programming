ddbfd0021162760a09432ef742c7b92a07365deb
DELSHEP <dicksonattu6@gmail.com>
2023-08-16 02:07:10 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a new line."
+ * return 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
